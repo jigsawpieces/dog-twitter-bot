5f0fa5fb75cb403ec548246117e0433e62eddfa6
Kathie <hellokathiewu@gmail.com>
2017-08-03 00:02:28 +0100

    indent

--- bot.js
+++ bot.js
@@ -5,66 +5,70 @@ greetings = require('./greetings')
 
 
 var client = new Twitter({
-	consumer_key: config.consumer_key,
-	consumer_secret: config.consumer_secret,
-	access_token_key: config.access_token_key,
-	access_token_secret: config.access_token_secret
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token_key: config.access_token_key,
+    access_token_secret: config.access_token_secret
 })
 
 
 // get a random dog from Dog API endpoint
-function getDog(result){
-	https = require('https')
-	url = 'https://dog.ceo/api/breeds/image/random'
+function getDog(result) {
+    https = require('https')
+    url = 'https://dog.ceo/api/breeds/image/random'
 
-	https.get(url, function(res){
-    body = ''
+    https.get(url, function(res) {
+        body = ''
 
-    res.on('data', function(chunk){
-        body += chunk
+        res.on('data', function(chunk) {
+            body += chunk
+        })
+
+        res.on('end', encodeImage)
     })
-
-   	res.on('end', encodeImage)
-	})
 }
 
 
 // encode the image to base64
-function encodeImage(result){		
-	imageURL = JSON.parse(body).message
-	base64.encode(imageURL, { string: true }, postImage)
+function encodeImage(result) {
+    imageURL = JSON.parse(body).message
+    base64.encode(imageURL, {
+        string: true
+    }, postImage)
 }
 
 
 // upload image to twitter
-function postImage(error, image){
-	if (error) {
-		console.log(error)
-	}
+function postImage(error, image) {
+    if (error) {
+        console.log(error)
+    }
 
-	client.post('media/upload', {media_data: image}, getTweet)
+    client.post('media/upload', {
+        media_data: image
+    }, getTweet)
 }
 
 
 // select random tweet to use from array
 function getTweet(error, media, result) {
-	greeting = greetings[Math.floor(Math.random()*greetings.length)]
+    greeting = greetings[Math.floor(Math.random() * greetings.length)]
 
-  if (!error) {
-    // Lets tweet it
-    var status = {
-      status: greeting,
-      media_ids: media.media_id_string // Pass the media id string
+    if (!error) {
+        // Lets tweet it
+        var status = {
+            status: greeting,
+            media_ids: media.media_id_string // Pass the media id string
+        }
+
+
+        // post the tweet
+        client.post('statuses/update', status, function(error, result) {
+            if (error) {
+                console.log(error)
+            }
+        })
     }
-
-
-    // post the tweet
-    client.post('statuses/update', status, function(error, result) {
-			if (error) {
-		    console.log(error)
-		  }
-		})
-  }
 }
 
 
